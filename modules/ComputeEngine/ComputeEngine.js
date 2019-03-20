4a4384ea1ac07b1cb91085d780666b0a90179097
Conor Turner <conorscturner@gmail.com>
2019-03-20 11:10:09 +0000

    - more cpu to help shutdown

--- modules/ComputeEngine/ComputeEngine.js
+++ modules/ComputeEngine/ComputeEngine.js
@@ -11,8 +11,8 @@ class ComputeEngine {
 			"--preemptible",
 			"--zone europe-west1-b",
 			"--container-image conorturner/bach-slave",
-			"--custom-cpu 2",
-			"--custom-memory 3GB",
+			"--custom-cpu 4",
+			"--custom-memory 4GB",
 			"--format json",
 			`--metadata shutdown-script="#! /bin/bash ${shutdownScript}"`,
 			envFlag
